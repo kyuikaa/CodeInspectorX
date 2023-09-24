b3635c9ebefbe8984fadeb36ca936ca21f4dbaa3
mikaaamoe <itzmerinz.dc@gmail.com>
2023-09-24 11:34:44 +0700

    refactor(main.js): adjust indentation for better code readability feat(main.js): createSettingsWindow function to open a new browser window for settings feat(main.js): add 'Settings' and 'Quit' options to File submenu in mainMenuTemplate feat(main.js): add loadApiKey and saveApiKey functions to manage API key settings feat(main.js): listen for 'save-settings' event to update and save API key feat(main.js): listen for 'analyze-code' event to send code for analysis and display result cleanup(main.js): remove unnecessary comments and console.log statements feat(main.js): set mainMenu as the application menu and load API key on app ready cleanup(main.js): fix indentation and remove extra whitespace feat(main.js): handle window-all-closed and activate events for macOS compatibility

--- main.js
+++ main.js
@@ -9,129 +9,129 @@ let settingsWindow;
 let apiKey = '';
 
 function createMainWindow() {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  });
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
 
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+    mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
-  mainWindow.on('closed', function () {
-    mainWindow = null;
-  });
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
 
-  if (process.env.NODE_ENV === 'development') {
-    mainWindow.webContents.openDevTools();
-  }
+    if (process.env.NODE_ENV === 'development') {
+        mainWindow.webContents.openDevTools();
+    }
 }
 
 function createSettingsWindow() {
-  settingsWindow = new BrowserWindow({
-    width: 400,
-    height: 300,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  });
+    settingsWindow = new BrowserWindow({
+        width: 400,
+        height: 300,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
 
-  settingsWindow.loadFile(path.join(__dirname, 'settings.html'));
+    settingsWindow.loadFile(path.join(__dirname, 'settings.html'));
 
-  settingsWindow.on('closed', function () {
-    settingsWindow = null;
-  });
+    settingsWindow.on('closed', function () {
+        settingsWindow = null;
+    });
 }
 
 const mainMenuTemplate = [
-  {
-    label: 'File',
-    submenu: [
-      {
-        label: 'Settings',
-        click() {
-          createSettingsWindow();
-        },
-      },
-      {
-        label: 'Quit',
-        accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
-        click() {
-          app.quit();
-        },
-      },
-    ],
-  },
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Settings',
+                click() {
+                    createSettingsWindow();
+                },
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click() {
+                    app.quit();
+                },
+            },
+        ],
+    },
 ];
 
 function loadApiKey() {
-  const settingsPath = path.join(app.getPath('userData'), 'settings.json');
-  if (fs.existsSync(settingsPath)) {
-    const settingsData = fs.readFileSync(settingsPath, 'utf-8');
-    const settings = JSON.parse(settingsData);
-    if (settings.apiKey) {
-      apiKey = settings.apiKey;
+    const settingsPath = path.join(app.getPath('userData'), 'settings.json');
+    if (fs.existsSync(settingsPath)) {
+        const settingsData = fs.readFileSync(settingsPath, 'utf-8');
+        const settings = JSON.parse(settingsData);
+        if (settings.apiKey) {
+            apiKey = settings.apiKey;
+        }
     }
-  }
 }
 
 function saveApiKey(apiKey) {
-  const settingsPath = path.join(app.getPath('userData'), 'settings.json');
-  const settings = { apiKey };
+    const settingsPath = path.join(app.getPath('userData'), 'settings.json');
+    const settings = { apiKey };
 
-  fs.writeFileSync(settingsPath, JSON.stringify(settings), 'utf-8');
+    fs.writeFileSync(settingsPath, JSON.stringify(settings), 'utf-8');
 }
 
 ipcMain.on('save-settings', (event, apiKeyInput) => {
-  apiKey = apiKeyInput;
-  saveApiKey(apiKey);
+    apiKey = apiKeyInput;
+    saveApiKey(apiKey);
 
-  if (settingsWindow) {
-    settingsWindow.close();
-  }
+    if (settingsWindow) {
+        settingsWindow.close();
+    }
 });
 
 ipcMain.on('analyze-code', async (event, code) => {
-  if (!apiKey) {
-    dialog.showErrorBox('API Key Missing', 'Please enter your OpenAI API key in settings.');
-    return;
-  }
+    if (!apiKey) {
+        dialog.showErrorBox('API Key Missing', 'Please enter your OpenAI API key in settings.');
+        return;
+    }
 
-  const gpt3 = new openai({
-    apiKey,
-    engine: 'davinci-codex',
-  });
-
-  try {
-    const response = await gpt3.completions.create({
-      prompt: code,
-      max_tokens: 100,
+    const gpt3 = new openai({
+        apiKey,
+        engine: 'davinci-codex',
     });
 
-    const analysisResult = response.choices[0].text;
-    mainWindow.webContents.send('analysis-result', analysisResult);
-  } catch (error) {
-    dialog.showErrorBox('Error Analyzing Code', 'An error occurred during code analysis.');
-    console.error('Error analyzing code:', error);
-  }
+    try {
+        const response = await gpt3.completions.create({
+            prompt: code,
+            max_tokens: 100,
+        });
+
+        const analysisResult = response.choices[0].text;
+        mainWindow.webContents.send('analysis-result', analysisResult);
+    } catch (error) {
+        dialog.showErrorBox('Error Analyzing Code', 'An error occurred during code analysis.');
+        console.error('Error analyzing code:', error);
+    }
 });
 
 app.on('ready', () => {
-  createMainWindow();
-  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-  Menu.setApplicationMenu(mainMenu);
-  loadApiKey();
+    createMainWindow();
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+    loadApiKey();
 });
 
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', function () {
-  if (mainWindow === null) {
-    createMainWindow();
-  }
+    if (mainWindow === null) {
+        createMainWindow();
+    }
 });
