64a88dc736de18641fc1072854f7fa3bf5f220a7
mikaaamoe <itzmerinz.dc@gmail.com>
2023-09-24 12:19:03 +0700

    refactor(renderer.js): update variable names and element IDs for clarity and consistency feat(renderer.js): add loading spinner and error message handling in analyzeButton eventListener feat(renderer.js): display analysis result or error message in respective event listeners

--- modules/renderer.js
+++ modules/renderer.js
@@ -1,14 +1,30 @@
 const { ipcRenderer } = require('electron');
 
-const codeInput = document.getElementById('codeInput');
-const analyzeButton = document.getElementById('analyzeButton');
-const analysisResult = document.getElementById('analysisResult');
+const codeInput = document.getElementById('code-input');
+const analyzeButton = document.getElementById('analyze-button');
+const resultContainer = document.getElementById('result-container');
+const loadingSpinner = document.getElementById('loading-spinner');
+const errorMessage = document.getElementById('error-message');
 
 analyzeButton.addEventListener('click', () => {
     const code = codeInput.value;
+    resultContainer.style.display = 'none';
+    errorMessage.style.display = 'none';
+    loadingSpinner.style.display = 'block';
+
     ipcRenderer.send('analyze-code', code);
 });
 
-ipcRenderer.on('analysis-result', (event, result) => {
-    analysisResult.textContent = result;
+ipcRenderer.on('analysis-result', (event, analysisResult) => {
+    loadingSpinner.style.display = 'none';
+    resultContainer.style.display = 'block';
+    errorMessage.style.display = 'none';
+    resultContainer.textContent = analysisResult;
+});
+
+ipcRenderer.on('analysis-error', (event, error) => {
+    loadingSpinner.style.display = 'none';
+    resultContainer.style.display = 'none';
+    errorMessage.style.display = 'block';
+    errorMessage.textContent = `An error occurred: ${error}`;
 });
