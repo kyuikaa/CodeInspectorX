0cd478f1ea618d13260604be7bd84f9502640db8
PlayCodeZ <putrii.cans05.orc@gmail.com>
2023-09-24 12:43:37 +0700

    fix(renderer.js): add copy button functionality to copy the content of result container to clipboard

--- modules/renderer.js
+++ modules/renderer.js
@@ -1,10 +1,12 @@
-const { ipcRenderer } = require('electron');
+const { ipcRenderer, clipboard } = require('electron');
 
 const codeInput = document.getElementById('code-input');
 const analyzeButton = document.getElementById('analyze-button');
 const resultContainer = document.getElementById('result-container');
 const loadingSpinner = document.getElementById('loading-spinner');
 const errorMessage = document.getElementById('error-message');
+const copyButton = document.getElementById('copy-button');
+const copySuccessMessage = document.getElementById('copy-success-message');
 
 analyzeButton.addEventListener('click', () => {
     const code = codeInput.value;
@@ -13,6 +15,16 @@ analyzeButton.addEventListener('click', () => {
     loadingSpinner.style.display = 'block';
 
     ipcRenderer.send('analyze-code', code);
+});
+
+copyButton.addEventListener('click', () => {
+    if (resultContainer.textContent) {
+        clipboard.writeText(resultContainer.textContent);
+        copySuccessMessage.style.display = 'block';
+        setTimeout(() => {
+            copySuccessMessage.style.display = 'none';
+        }, 2000);
+    }
 });
 
 ipcRenderer.on('analysis-result', (event, analysisResult) => {
