e1ee331381f541ff6cf64f300672cf09a3549da9
mikaaamoe <mikaaa.moe>
2023-09-24 11:21:01 +0700

    chore(main.js): refactor code for creating main and settings windows feat(main.js): add menu template and functionality for opening settings window and quitting the app feat(main.js): add functions to load and save API key from settings file feat(main.js): add IPC event listener for saving settings and analyzing code fix(main.js): change max_tokens value to 100 in code analysis IPC event fix(main.js): change engine field to 'davinci-codex' in code analysis IPC event fix(main.js): show error dialog if API key is missing when analyzing code fix(main.js): show error dialog if an error occurs during code analysis

--- main.js
+++ main.js
@@ -1,60 +1,137 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, Menu, dialog } = require('electron');
 const path = require('path');
+const fs = require('fs');
 const openai = require('openai');
 
-const apiKey = 'YOUR_OPENAI_API_KEY'; // Replace with your OpenAI API key
-
 let mainWindow;
+let settingsWindow;
 
-function createWindow() {
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true,
-        },
-    });
+let apiKey = '';
 
-    mainWindow.loadFile(path.join(__dirname, 'index.html'));
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
 
-    mainWindow.on('closed', function () {
-        mainWindow = null;
-    });
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.webContents.openDevTools();
+  }
 }
 
-app.on('ready', createWindow);
+function createSettingsWindow() {
+  settingsWindow = new BrowserWindow({
+    width: 400,
+    height: 300,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
 
-app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
+  settingsWindow.loadFile(path.join(__dirname, 'settings.html'));
 
-app.on('activate', function () {
-    if (mainWindow === null) {
-        createWindow();
+  settingsWindow.on('closed', function () {
+    settingsWindow = null;
+  });
+}
+
+const mainMenuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Settings',
+        click() {
+          createSettingsWindow();
+        },
+      },
+      {
+        label: 'Quit',
+        accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click() {
+          app.quit();
+        },
+      },
+    ],
+  },
+];
+
+function loadApiKey() {
+  const settingsPath = path.join(app.getPath('userData'), 'settings.json');
+  if (fs.existsSync(settingsPath)) {
+    const settingsData = fs.readFileSync(settingsPath, 'utf-8');
+    const settings = JSON.parse(settingsData);
+    if (settings.apiKey) {
+      apiKey = settings.apiKey;
     }
+  }
+}
+
+function saveApiKey(apiKey) {
+  const settingsPath = path.join(app.getPath('userData'), 'settings.json');
+  const settings = { apiKey };
+
+  fs.writeFileSync(settingsPath, JSON.stringify(settings), 'utf-8');
+}
+
+ipcMain.on('save-settings', (event, apiKeyInput) => {
+  apiKey = apiKeyInput;
+  saveApiKey(apiKey);
+
+  if (settingsWindow) {
+    settingsWindow.close();
+  }
 });
 
 ipcMain.on('analyze-code', async (event, code) => {
-    const gpt3 = new openai({
-        apiKey,
-        model: 'YOUR_GPT3_MODEL', // Replace with your preferred GPT-3 model
+  if (!apiKey) {
+    dialog.showErrorBox('API Key Missing', 'Please enter your OpenAI API key in settings.');
+    return;
+  }
+
+  const gpt3 = new openai({
+    apiKey,
+    engine: 'davinci-codex',
+  });
+
+  try {
+    const response = await gpt3.completions.create({
+      prompt: code,
+      max_tokens: 100,
     });
 
-    try {
-        // Make an API call to OpenAI for code analysis
-        const response = await gpt3.completions.create({
-            engine: 'davinci-codex', // Use the Codex engine for code-related tasks
-            prompt: code,
-            max_tokens: 100, // Adjust as needed
-        });
+    const analysisResult = response.choices[0].text;
+    mainWindow.webContents.send('analysis-result', analysisResult);
+  } catch (error) {
+    dialog.showErrorBox('Error Analyzing Code', 'An error occurred during code analysis.');
+    console.error('Error analyzing code:', error);
+  }
+});
 
-        // Extract and send the analysis result back to the renderer process
-        const analysisResult = response.choices[0].text;
-        mainWindow.webContents.send('analysis-result', analysisResult);
-    } catch (error) {
-        console.error('Error analyzing code:', error);
-        event.reply('analysis-result', 'An error occurred during analysis.');
-    }
+app.on('ready', () => {
+  createMainWindow();
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  Menu.setApplicationMenu(mainMenu);
+  loadApiKey();
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createMainWindow();
+  }
 });
